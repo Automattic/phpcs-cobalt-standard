54d91567b69a5b5c6668610945829f7d7c207d7f
Del Putnam <del@putnams.net>
2018-01-14 17:59:11 -0500

    Rename directory to Cobalt

---
+++ CobaltStandard/SniffHelpers.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace CobaltStandard;
+
+use PHP_CodeSniffer\Files\File;
+
+class SniffHelpers {
+	public function isFunctionCall(File $phpcsFile, $stackPtr) {
+		$tokens = $phpcsFile->getTokens();
+		$nextNonWhitespacePtr = $phpcsFile->findNext(T_WHITESPACE, $stackPtr + 1, null, true, null, false);
+		// if the next non-whitespace token is not a paren, then this is not a function call
+		if ($tokens[$nextNonWhitespacePtr]['type'] !== 'T_OPEN_PARENTHESIS') {
+			return false;
+		}
+		// if the previous non-whitespace token is a function, then this is not a function call
+		$prevNonWhitespacePtr = $phpcsFile->findPrevious(T_WHITESPACE, $stackPtr - 1, null, true, null, false);
+		if ($tokens[$prevNonWhitespacePtr]['type'] === 'T_FUNCTION') {
+			return false;
+		}
+		return true;
+	}
+
+	// From https://stackoverflow.com/questions/619610/whats-the-most-efficient-test-of-whether-a-php-string-ends-with-another-string
+	public function doesStringEndWith(string $string, string $test): bool {
+		$strlen = strlen($string);
+		$testlen = strlen($test);
+		if ($testlen > $strlen) {
+			return false;
+		}
+		return substr_compare($string, $test, $strlen - $testlen, $testlen) === 0;
+	}
+}
---
+++ CobaltStandard/Sniffs/StrictTypes/RequireStrictTypesSniff.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace CobaltStandard\Sniffs\StrictTypes;
+
+use PHP_CodeSniffer\Sniffs\Sniff;
+use PHP_CodeSniffer\Files\File;
+
+class RequireStrictTypesSniff implements Sniff {
+	public function register() {
+		return [T_OPEN_TAG];
+	}
+
+	public function process(File $phpcsFile, $stackPtr) {
+		if (! $this->hasInitialDeclare($phpcsFile, $stackPtr)
+			|| ! $this->isInitialDeclareStrictTypes($phpcsFile, $stackPtr)
+			|| ! $this->isInitialDeclareStrictTypesOn($phpcsFile, $stackPtr)) {
+			$this->addStrictTypeError($phpcsFile, $stackPtr);
+		}
+	}
+
+	private function hasInitialDeclare($phpcsFile, $stackPtr) {
+		$tokens = $phpcsFile->getTokens();
+		$declarePtr = $phpcsFile->findNext(T_DECLARE, $stackPtr, null, false, null, true);
+		return ($declarePtr && isset($tokens[$declarePtr]));
+	}
+
+	private function isInitialDeclareStrictTypes($phpcsFile, $stackPtr) {
+		$tokens = $phpcsFile->getTokens();
+		$declarePtr = $phpcsFile->findNext(T_DECLARE, $stackPtr, null, false, null, true);
+		$declareStringPtr = $phpcsFile->findNext(T_STRING, $declarePtr, null, false, null, true);
+		$declareStringToken = $tokens[$declareStringPtr];
+		return ($declareStringToken && $declareStringToken['content'] === 'strict_types');
+	}
+
+	private function isInitialDeclareStrictTypesOn($phpcsFile, $stackPtr) {
+		$tokens = $phpcsFile->getTokens();
+		$declarePtr = $phpcsFile->findNext(T_DECLARE, $stackPtr, null, false, null, true);
+		$declareNumPtr = $phpcsFile->findNext(T_LNUMBER, ($declarePtr + 1), null, false, null, true);
+		$declareNumToken = $tokens[$declareNumPtr];
+		return ($declareNumToken && $declareNumToken['content'] === '1');
+	}
+
+	private function addStrictTypeError(File $phpcsFile, $stackPtr) {
+		$error = 'File must start with a strict types declaration';
+		$phpcsFile->addError($error, $stackPtr, 'StrictTypes');
+	}
+}
